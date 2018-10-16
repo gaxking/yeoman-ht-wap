0543292353f95064332248e4317240d5dc499e2e
gax <gaxking@gmail.com>
2018-10-16 15:18:21 +0800

    delete

--- generators/app/templates/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-var HT_ENV = '"' + process.env.HT_ENV + '"';
-module.exports = {
-  NODE_ENV: '"production"',
-  HT_ENV: HT_ENV
-}
--- generators/app/templates/src/lib/animation/store/actions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2017/6/5.
- */
-import * as types from './types'
-
-export default {
-
-}
